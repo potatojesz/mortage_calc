b0258ab00eee9fe2086eaa96627f805d2b20af02
iid <iid@iid.clear2pay.com>
2019-07-17 15:18:02 +0200

    Fix bug with import

--- src/main/java/com/tklimczak/mortagecalc/config/swagger/SwaggerConfig.java
+++ src/main/java/com/tklimczak/mortagecalc/config/swagger/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.tklimczak.mortagecalc.config.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
