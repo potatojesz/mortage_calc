f0e7eb5e32f4d93d5aa127627e6a194746f8f788
iid <iid@iid.clear2pay.com>
2019-07-19 12:35:01 +0200

    Added contextTest

--- src/test/java/com/tklimczak/mortagecalc/MortageCalcApplicationTests.java
+++ src/test/java/com/tklimczak/mortagecalc/MortageCalcApplicationTests.java
@@ -1,7 +1,12 @@
 package com.tklimczak.mortagecalc;
 
+import com.tklimczak.mortagecalc.controllers.MortageCalculatorController;
+import com.tklimczak.mortagecalc.services.InstallmentsCalculatorService;
+import com.tklimczak.mortagecalc.services.OverpaymentCalculatorService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +14,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MortageCalcApplicationTests {
 
+	@Autowired
+	private MortageCalculatorController mortageCalculatorController;
+	@Autowired
+	private InstallmentsCalculatorService installmentsCalculatorService;
+	@Autowired
+	private OverpaymentCalculatorService overpaymentCalculatorService;
+
 	@Test
 	public void contextLoads() {
+		Assert.assertNotNull(mortageCalculatorController);
+		Assert.assertNotNull(installmentsCalculatorService);
+		Assert.assertNotNull(overpaymentCalculatorService);
 	}
 
 }
