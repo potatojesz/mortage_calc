f7f3bf8c855ca4671fb7a55b774fe9c35bcf83e2
iid <iid@iid.clear2pay.com>
2019-07-19 14:34:54 +0200

    Added API info for Swagger and disable models section

--- src/main/java/com/tklimczak/mortagecalc/config/swagger/SwaggerConfig.java
+++ src/main/java/com/tklimczak/mortagecalc/config/swagger/SwaggerConfig.java
@@ -3,10 +3,15 @@ package com.tklimczak.mortagecalc.config.swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -18,6 +23,23 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
+                .build().apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Mortage Calculation REST API")
+                .description("You can calculate parameters of your mortage, and what it will look like in the even of overpayment.")
+                .license("Apache 2.0")
+                .contact(new Contact("Tomasz Klimczak", "https://github.com/potatojesz", "potatojesz@gmail.com"))
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
+
+    @Bean
+    public UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .defaultModelsExpandDepth(-1)
                 .build();
     }
 }
--- src/main/java/com/tklimczak/mortagecalc/controllers/MortageCalculatorController.java
+++ src/main/java/com/tklimczak/mortagecalc/controllers/MortageCalculatorController.java
@@ -2,19 +2,17 @@ package com.tklimczak.mortagecalc.controllers;
 
 import com.tklimczak.mortagecalc.domain.models.Mortage;
 import com.tklimczak.mortagecalc.domain.models.MortageResult;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.tklimczak.mortagecalc.domain.models.OverpaymentRequest;
 import com.tklimczak.mortagecalc.domain.models.OverpaymentResult;
 import com.tklimczak.mortagecalc.services.InstallmentsCalculatorService;
 import com.tklimczak.mortagecalc.services.OverpaymentCalculatorService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@Api(value="Mortage calculation operations")
 @RestController
 @RequestMapping("/mortage")
 public class MortageCalculatorController {
@@ -24,6 +22,7 @@ public class MortageCalculatorController {
 	@Autowired
 	private transient InstallmentsCalculatorService installmentsService;
 
+	@ApiOperation(value = "Calculate overpay for you mortage", response = OverpaymentResult.class)
 	@CrossOrigin
 	@PostMapping("/overpay")
 	ResponseEntity<OverpaymentResult> calculateOverpayment(@RequestBody OverpaymentRequest request) {
@@ -34,6 +33,7 @@ public class MortageCalculatorController {
 		return ResponseEntity.ok(result);
 	}
 
+	@ApiOperation(value = "Calculate installments and interests for you mortage", response = MortageResult.class)
 	@CrossOrigin
 	@PostMapping("/calc")
 	ResponseEntity<MortageResult> calculateMortage(@RequestBody Mortage mortage) {
